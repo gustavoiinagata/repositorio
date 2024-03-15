b967d6a5033ebde861b1185e9f8fc0175f0fa4df
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-03-15 11:34:26 -0300

    .

--- jogo.js
+++ jogo.js
@@ -3,9 +3,10 @@ let numeroSecreto = 8;
 let chute = prompt ('escolha o seu rapper favorito');
 
 if(numeroSecreto == chute){
+    alert('Esse aí é pika');
+    console.log('continua navegando aí kdot')
+} else{
     alert('Esse aí é meio ruim na real, mas fds');
-    console.log('continua navegando aí seu merda')
-} else{alert('Esse aí é meio ruim na real, mas fds');
 console.log('continua navegando aí seu merda')
 
 }
