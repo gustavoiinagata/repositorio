da796605d0be161297a2a57a3104cdcff2159e41
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-04-12 08:58:53 -0300

    ytyy

--- cont.js
+++ cont.js
@@ -1,13 +1,15 @@
 const botoes = document.querySelectorAll( '.objetivos ');
-
+const textos = document.querySelectorAll( '.aba-conteudo');
 
 for (let x = 0; x<botoes. length; x++){
     console.log(x);
     botoes[x].onclick = fuction () {
     for(let y= 0; y< botoes.length; y++){
-        botoes[y].classList.remove('atvo');
+        botoes[y].classList.remove('ativo');
+        textos[y].classList.remove('ativa');
     }
-        botoes[x].classList.add('atvo');
+        botoes[x].classList.add('ativo');
+        textos[x].classList.add('ativa');
 }
 //botoes[x].classList.remove('atvo');}
 }
