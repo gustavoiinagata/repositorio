c233d61ed6a2a845dc47cb36034200df3916dd58
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-04-12 08:30:49 -0300

    ccv

--- cont.js
+++ cont.js
@@ -1,14 +1,14 @@
-const botos = document.querySelectorAll( '.objetivos ');
+const botoes = document.querySelectorAll( '.objetivos ');
 
 
 for (let x = 0; x<botoes. length; x++){
     console.log(x);
-    botoes [x].onclick = fuction () {
+    botoes[x].onclick = fuction () {
     for(let y= 0; y< botoes.length; y++){
-        botoes [y].classList.remove('atvo');
+        botoes[y].classList.remove('atvo');
     }
-        botoes [x].classList.add('atvo');}
+        botoes[x].classList.add('atvo');
 }
-//botoes [x].classList.remove('atvo');}
+//botoes[x].classList.remove('atvo');}
 }
 
