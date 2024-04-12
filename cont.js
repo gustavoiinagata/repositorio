e2be149e01b376908e34f96e2658d300ad0d3205
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-04-12 09:03:08 -0300

    qwf

--- cont.js
+++ cont.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll( '.aba-conteudo');
 
 for (let x = 0; x<botoes. length; x++){
     console.log(x);
-    botoes[x].onclick = fuction () {
+    botoes[x].onclick = function () {
     for(let y= 0; y< botoes.length; y++){
         botoes[y].classList.remove('ativo');
         textos[y].classList.remove('ativa');
