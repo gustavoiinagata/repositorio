efe423eedf456bdf68d3642b599ddb91d3f72f5d
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-04-26 09:08:46 -0300

    8

--- cont.js
+++ cont.js
@@ -21,7 +21,7 @@ let tempoObjetivo2 = new Date ('2024-04-30T00:00:00');
 let tempoObjetivo3 = new Date ('2024-05-28T00:00:00');
 let tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
 
-let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
