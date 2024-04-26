7523429f6c0da36999c692914dd54056315fafa0
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-04-26 08:40:39 -0300

    ww

--- cont.js
+++ cont.js
@@ -16,3 +16,7 @@ for (let x = 0; x<botoes. length; x++){
 
 const contadores = document. querySelectorAll('.contador');
 let tempoAtual = new Date ();
+let tempoObjetivo1 = newDate (' 2024-04-28T00:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+contadores [0]. textContent = tempoFinal;
